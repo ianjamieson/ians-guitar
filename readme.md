5b51fb4459daa92e1c0ca83554adce4707965a1a
Ian Jamieson <ian@tixel.com>
2022-08-13 19:38:32 +0100

    update

---
+++ readme.md
@@ -0,0 +1,8 @@
+## todo
+- Record audio
+- Do photos
+- Add actual image
+- Add actual audio
+- Add meta description
+- Add correct details
+- add site to google search console, need to verify
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ['./docs/**/*.{html,js}'],
   theme: {
-    extend: {},
+    colors: {
+      black: 'black',
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'orange': {
+        50: '#fce6e1',
+        200: '#ecaea0',
+        500: '#d88b79',
+      },
+      'gray': {
+        50: '#f7f7f7',
+        100: '#f0f0f0',
+        200: '#d9d9d9',
+        300: '#c3c3c3',
+        400: '#959595',
+        500: '#686868',
+        600: '#5e5e5e',
+        700: '#4e4e4e',
+        800: '#3e3e3e',
+        900: '#333333',
+      },
+    },
+    extend: {
+      maxWidth: {
+        container: '1024px'
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
