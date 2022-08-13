886b10650fd395b8595aeea5df1ac8cb4fa76725
Ian Jamieson <ian@tixel.com>
2022-08-13 19:29:47 +0100

    Updates

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,10 +5,12 @@ class AudioPlayer {
         this.player = player;
         this.timer = null;
         this.playButton = this.player.querySelector('[data-control="play"]');
+        this.stopButton = this.player.querySelector('[data-control="stop"]');
         this.progressBar = this.player.querySelector('[data-control="progress"]');
         this.completionPercentage = '0%';
 
-        this.playButton.addEventListener('click', this.playPause.bind(this));
+        this.playButton.addEventListener('click', this.play.bind(this));
+        this.stopButton.addEventListener('click', this.pause.bind(this));
         this.audio.addEventListener('ended', this.pause.bind(this));
 
     }
@@ -21,15 +23,17 @@ class AudioPlayer {
     }
     play() {
         this.audio.play();
-        this.playButton.innerHTML = '⬛';
+        this.playButton.classList.add('hidden');
+        this.stopButton.classList.remove('hidden');
         this.timer = setInterval(() => {
             this.completionPercentage = ((this.audio.currentTime / Math.round(this.audio.duration)) * 100) + '%';
             this.progressBar.style.width = this.completionPercentage;
         }, 30);
     }
     pause() {
-        this.audio.pause();
-        this.playButton.innerHTML = '&#9658;';
+        this.audio.pause()
+        this.playButton.classList.remove('hidden');
+        this.stopButton.classList.add('hidden');;
         clearInterval(this.timer);
     }
 }
